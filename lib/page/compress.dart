0c7c648db948cdc7e67601645870da0c5e38a545
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-04-25 15:51:10 +0530

    with resolution

--- lib/page/compress.dart
+++ lib/page/compress.dart
@@ -1,9 +1,9 @@
-
-
 import 'dart:io';
+import 'dart:ui' as ui;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:imagecompressorandresizer/utils/color.dart';
 
@@ -15,38 +15,50 @@ class CompressPage extends StatefulWidget {
 }
 
 class _CompressPageState extends State<CompressPage> {
-  final ImagePicker imagePicker=ImagePicker();
-  List<XFile> imageFileList=[];
-  List<String> imageSizes=[];
-  int currentPageIndex=0;
+  final ImagePicker imagePicker = ImagePicker();
+  List<XFile> imageFileList = [];
+  List<String> imageSizes = [];
+  List<String> imageResolutions = [];
+  int currentPageIndex = 0;
 
   // Multiple Image Picker
-  void selectImage()async{
-    final List<XFile>? selectedImages=await imagePicker.pickMultiImage();
-    if(selectedImages!.isNotEmpty){
+  void selectImage() async {
+    final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
+    if (selectedImages!.isNotEmpty) {
       imageFileList.addAll(selectedImages);
 
-      for(var file in selectedImages){
-        File image=File(file.path);
-        int sizeInBytes=image.lengthSync();
-        double sizeInKB=sizeInBytes/1024;
+      for (var file in selectedImages) {
+        File image = File(file.path);
+        int sizeInBytes = image.lengthSync();
+        double sizeInKB = sizeInBytes / 1024;
         String sizeText;
-        if(sizeInKB >1024){
-          double sizeInMB=sizeInKB/1024;
-          sizeText="${sizeInMB.toStringAsFixed(2)} MB";
-        }else{
-          sizeText="${sizeInKB.toStringAsFixed(2)} KB";
+        if (sizeInKB > 1024) {
+          double sizeInMB = sizeInKB / 1024;
+          sizeText = "${sizeInMB.toStringAsFixed(2)} MB";
+        } else {
+          sizeText = "${sizeInKB.toStringAsFixed(2)} KB";
         }
         imageSizes.add(sizeText);
+
+        // Fetching image resolution
+        ui.Image resolvedImage =
+            await decodeImageFromList(await image.readAsBytes());
+        imageResolutions.add('${resolvedImage.width}x${resolvedImage.height}');
       }
     }
-    setState(() {
-    });
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    imageSizes = [];
+    imageResolutions = [];
   }
 
   @override
   Widget build(BuildContext context) {
-    final size=MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Compress Image"),
@@ -123,10 +135,12 @@ class _CompressPageState extends State<CompressPage> {
       //     ],
       //   ),
       // ),
-      body:SafeArea(
+      body: SafeArea(
         child: Column(
           children: [
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Container(
               height: size.height / 3,
               color: Colors.green,
@@ -139,36 +153,67 @@ class _CompressPageState extends State<CompressPage> {
                   },
                   itemBuilder: (BuildContext context, int index) {
                     return Image.file(
-                      File(imageFileList[index].path,),
+                      File(
+                        imageFileList[index].path,
+                      ),
                       fit: BoxFit.cover,
                     );
-                  }
-              ),
+                  }),
             ),
-            imageFileList !.isNotEmpty ? Container(
-              padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-              color: Colors.yellow,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Selected", style: TextStyle(fontSize: 16),),
-                      Text("${imageFileList.length} images", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-
-                      Text("Size", style: TextStyle(fontSize: 16),),
-                      Text("${imageSizes.isNotEmpty && currentPageIndex < imageSizes.length ? imageSizes[currentPageIndex] : ''}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),)
-                    ],
-                  ),
-                ],
-              ),
-            ): SizedBox(),
+            imageFileList!.isNotEmpty
+                ? Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    color: Colors.yellow,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Selected",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              "${imageFileList.length} images",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Size",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              "${imageSizes.isNotEmpty && currentPageIndex < imageSizes.length ? imageSizes[currentPageIndex] : ''}",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        // Column(
+                        //   crossAxisAlignment: CrossAxisAlignment.start,
+                        //   children: [
+                        //     Text(
+                        //       "Resolution",
+                        //       style: TextStyle(fontSize: 16),
+                        //     ),
+                        //     Text(
+                        //       "${imageResolutions.isNotEmpty && currentPageIndex < imageResolutions.length ? imageResolutions[currentPageIndex] : ''}",
+                        //       style: TextStyle(
+                        //           fontSize: 16, fontWeight: FontWeight.bold),
+                        //     ),
+                        //   ],
+                        // ),
+                      ],
+                    ),
+                  )
+                : SizedBox(),
 
             SizedBox(
                 width: size.width,
@@ -176,13 +221,15 @@ class _CompressPageState extends State<CompressPage> {
                     style: ElevatedButton.styleFrom(
                         backgroundColor: backgroundColor,
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero
-                        )
-                    ),
+                            borderRadius: BorderRadius.zero)),
                     onPressed: selectImage,
-                    child: Text("Select Image", style: TextStyle(fontSize: 18, color: Colors.white),)
-                )
+                    child: Text(
+                      "Select Image",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ))
             ),
+            Text("Select Compress ",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
+            
           ],
         ),
       ),
